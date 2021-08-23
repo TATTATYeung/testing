d63727ab4c53c60994e745178138bb05a3575ade
TATTATYeung <89256474+TATTATYeung@users.noreply.github.com>
2021-08-24 03:01:05 +0800

    2

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import { useHistory } from "react-router-dom";
 import { Input, message, Button } from "antd";
 import "antd/dist/antd.css";
 import "./index.css";
 
 const App = () => {
   const [date, setDate] = useState(null);
+  let history = useHistory();
   const handleChange = (event) => {
     // String value = document.getElementById("dataSelection").value;
     const value = document.getElementById("dataSelection").value;
     message.info(`Selected Date: ${value ? value : "None"}`);
     setDate(value);
+
+    this.history.push("/search");
   };
   return (
     <div style={{ width: 500, margin: "100px auto" }}>
       <Input id="dataSelection" />
       <br></br>
-      <Button type="primary" onClick={handleChange}>
+      <Button type="primary" onClick={handleChange} history={this.history}>
         Click
       </Button>
-      <div style={{ marginTop: 16 }}>Selected Date: {date ? date : "None"}</div>
+      <div style={{ marginTop: 16 }}>
+        Selected Date2: {date ? date : "None"}
+      </div>
     </div>
   );
 };
